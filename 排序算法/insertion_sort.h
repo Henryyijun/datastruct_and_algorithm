a0b113116f784383d2ccd505052886ca4380db18
henry <1421745355@qq.com>
2019-05-10 18:04:27 +0800

    add insertion_sort

---
+++ 排序算法/insertion_sort.h
@@ -0,0 +1,11 @@
+void insertion_sort(int *a, int len) {
+    for (int i = 1; i < len; i++) {
+        int temp = a[i];
+        int j = i - 1;
+        while (j >= 0 && a[j] > temp) {
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j + 1] = temp;
+    }
+}
--- 排序算法/main.cpp
+++ 排序算法/main.cpp
@@ -3,8 +3,10 @@ using namespace std;
 
 int main(int arg, char** argv) {
     int a[num] = {2, 1, 3, 1, 0};
-    //bubble_sort(a, num);
-    show_array(a, num);
+    /*bubble_sort(a, num);
+   
     bubble_sort_op(a, num);
+    show_array(a, num);*/
+    quick_sort(a, 0, num - 1);
     show_array(a, num);
 }
